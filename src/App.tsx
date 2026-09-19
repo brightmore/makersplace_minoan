947d12d78439bcf653140fa1ea1d0dd3f18226ac
brightmore <brightmore1@gmail.com>
2026-09-19 19:12:44 +0000

    style: comment out unused Sparkles import in App component

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import { Footer } from './components/Footer';
 import { RegistrationModal } from './components/RegistrationModal';
 import { ScrollToTop } from './components/ScrollToTop';
 import { SportId } from './types';
-// import { Sparkles } from 'lucide-react';
 import { AuthProvider } from './context/AuthContext';
 
 // Individual Page Components
